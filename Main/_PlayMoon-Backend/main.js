3495593a562d39327ff2dd61f3a5bf8c6010c418
Ivan Opacak <sandrostross310@gmail.com>
2021-12-28 20:53:27 +0100

    Mongodb eingeführt

--- Main/_PlayMoon-Backend/main.js
+++ Main/_PlayMoon-Backend/main.js
@@ -1,35 +1,38 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 
-var dotenv = require('dotenv').config({
+const dotenv = require('dotenv').config({
     path: 'environment/.env'
 })
-var bodyParser = require('body-parser')
-var cookieParser = require('cookie-parser')
-var cors = require('cors')({
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')({
     origin: '*'
 })
+const mongoose = require("mongoose")
+const PORT = process.env.PORT | 5000
+    //ROUTES--START
 
-//ROUTES--START
-
-var routeRegister = require('./routing/register')
+let routeRegister = require('./routing/register')
 
 //ROUTES--END
 
 //--API--START
 
-var db = require('./api/db')
+/*---------------------*/
 
 //--API--END--
 
+//--USE--
 app.use(routeRegister)
 
 app.use(cors);
 app.use(bodyParser('t'))
 app.use(cookieParser('t'))
+    //--USE-END
 
-var PORT = process.env.PORT | 5000
-
-app.listen(PORT, () => console.log(`--Backend running on port: ${PORT}--`))
-
-app.use()
+//Connect to mongodb
+let dbURI = 'mongodb+srv://admin:PQ9KKqflvgdCcqRb@cluster0.rmzdz.mongodb.net/playmoon?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => app.listen(PORT, () => console.log(`--Backend running on port: ${PORT}--`)))
+    .catch(err => console.log(err, "!"))
