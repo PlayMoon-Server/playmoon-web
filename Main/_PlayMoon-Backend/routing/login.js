480f355afb2f8edb50012142922221421f9c4076
Ivan Opacak <sandrostross310@gmail.com>
2022-01-02 22:28:09 +0100

    login & register routes

--- Main/_PlayMoon-Backend/routing/login.js
+++ Main/_PlayMoon-Backend/routing/login.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const Verify = require('../api/db/models/verify')
 const hashString = require('../api/security/hashString')
 
 //Routing via express in mehrern dateien zu einer main.js zusammengestellt
 
 //Post request:
-router.post('/auth/register', (req, res) => {
+router.post('/auth/login', (req, res) => {
     //Cooming soon
 })
 
