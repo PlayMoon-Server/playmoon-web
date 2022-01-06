73cbdd9f9b8e4d3052701a520acc3659bf05df68
Ivan Opacak <sandrostross310@gmail.com>
2022-01-06 19:36:21 +0100

    check pw is valid (security)

---
+++ Main/_PlayMoon-Backend/api/services/checkPw.js
@@ -0,0 +1,13 @@
+module.exports = async(pw, pw2) => {
+    var numbers = /[0-9]/g;
+    var upperCaseLetters = /[A-Z]/g;
+
+    var errorObject = { err: false, error: null }
+
+    if (pw != pw2) errorObject = { err: true, error: "Deine Passwörter stimmen nicht überein" }
+    else if (pw.length < 8) errorObject = { err: true, error: "Dein Passwort muss mindestens 8 Zeichen enthalten" }
+    else if (numbers.test(pw) != true) errorObject = { err: true, error: "Dein Passwort muss mindestens 1 Zahl enthalten" }
+    else if (upperCaseLetters.test(pw) != true) errorObject = { err: true, error: "Dein Passwort muss mindestens 1 Großbuchstaben enthalten" }
+
+    return errorObject
+}
