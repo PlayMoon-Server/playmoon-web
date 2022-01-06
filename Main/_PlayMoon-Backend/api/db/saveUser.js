d1838146c8894b47c70dc9ca69fce21585b261c6
Ivan Opacak <sandrostross310@gmail.com>
2022-01-06 19:35:25 +0100

    saveUser funktion fertig

--- Main/_PlayMoon-Backend/api/db/saveUser.js
+++ Main/_PlayMoon-Backend/api/db/saveUser.js
@@ -1,13 +1,45 @@
-const Account = require("../db/models/account")
+const User = require('./models/user_account')
 
-module.exports = async(user) => {
-    return new Promise((res, rej) => {
-        Account.create(user)
-            .then(result => {
-                res(result)
-            })
-            .catch(err => {
-                rej(err)
-            })
-    })
+//security
+
+const hashString = require('../security/hashString')
+const passwordSalting = require('../security/passwordSalting')
+const checkPw = require('../services/checkPw')
+
+//
+
+module.exports = async(verifyToken, password, password2) => {
+    var pwChecked = await checkPw(password, password2)
+    try {
+        //CHECK IF TOKEN IS VALID
+
+        const userExists = await User.exists({ verifyToken: verifyToken })
+        if (!userExists) return { err: true, error: "Dieser Verifytoken ist ungültig" }
+
+        //--
+
+        //USER EXISTS - (check if alredy has a pw) qwda1
+
+        const user = await User.findOne({ verifyToken: verifyToken })
+        if (user.password) return { err: true, error: "Dieser Verifytoken wurde bereits benutzt" }
+
+        //--
+
+        //CHECK IF PW IS Ok
+
+        if (pwChecked.err) return { err: true, error: pwChecked.error }
+
+        //--
+
+        //SET PASSWORD OF USER WHERE VERIFYTOKEN=VERIFYTOKEN
+        try {
+            await User.updateOne({ verifyToken: verifyToken }, { password: hashString(passwordSalting(password)) }).exec()
+            return { err: false, error: null }
+        } catch (err) {
+            return { err: true, error: "Die Daten konnten nicht abgespeichert werden, wende dich an den Support" }
+        }
+
+    } catch (err) {
+        return { err: true, error: err }
+    }
 }
